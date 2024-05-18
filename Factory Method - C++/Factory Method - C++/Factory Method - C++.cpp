820a9ea337f0f135cd0752916eec95746898eff3
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-18 18:09:03 +0200

    Add files via upload

---
+++ Factory Method - C++/Factory Method - C++/Factory Method - C++.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+__interface IProduction {
+    virtual void Release() = 0;
+};
+
+class Car : public IProduction {
+public:
+    virtual void Release() override {
+        cout << "New passenger car\n";
+    }
+};
+
+class Truck : public IProduction {
+public:
+    virtual void Release() override {
+        cout << "New truck\n";
+    }
+};
+
+__interface IWorkShop {
+    virtual IProduction* Create() = 0;
+};
+
+class CarWorkShop : public IWorkShop {
+public:
+    virtual IProduction* Create() override {
+        return new Car();
+    }
+};
+
+class TruckWorkShop : public IWorkShop {
+public:
+    virtual IProduction* Create() override {
+        return new Truck();
+    }
+};
+
+int main() {
+    IWorkShop* creator = new CarWorkShop;
+    IProduction* car = creator->Create();
+    creator = new TruckWorkShop();
+    IProduction* truck = creator->Create();
+    car->Release();
+    truck->Release();
+}
